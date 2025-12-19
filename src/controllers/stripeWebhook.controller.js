783f885f407b3ae63fb5f24972f26d791dab248e
Sandeep Mehta <sandeep797671@gmail.com>
2025-12-19 20:53:57 +0530

    correct event type in stripe webhook

--- src/controllers/stripeWebhook.controller.js
+++ src/controllers/stripeWebhook.controller.js
@@ -23,7 +23,7 @@ const stripeWebhooks = asyncHandler(async (req, res) => {
     return res.status(400).send(`Webhook Error: ${error.message}`);
   }
 
-  if (event.type === "checkout.session.completed") {
+  if (event.type === "payment_intent.succeeded") {
     const session = event.data.object;
 
     const bookingId = session.metadata?.bookingId;
